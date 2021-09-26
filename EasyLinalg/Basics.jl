fadb5a58f8a72352078fce1772a3b5d82aaaf504
Bruno Hryniewicz <brunohry@dcc.ufrj.br>
2021-09-25 22:47:55 -0300

    Add point2d and Fix product

--- EasyLinalg/Basics.jl
+++ EasyLinalg/Basics.jl
@@ -1,38 +1,48 @@
+include("../Types/Signal/SignalDefinitions.jl")
+include("../Types/Point2D/Point2DDefinitions.jl")
+
 numberTypes = Union{Integer, Float64}
-types = Union{Signal}
+types = Union{Signal, Point2D}
 
 
 function convex_combination(B,increment)
     x=1:-increment:0
     y=0:increment:1
-    C=[x' ; y']
-    A=B*C
+    C=[x'; y']
+    A = B * C
     return A
 end
 
 function *(S::Vector{<:types}, w::Vector{<:numberTypes})
     T = toNumberMatrix(S)
     T = T * w
-    K = copy(S[1])
+    K = deepcopy(S[1])
     s = size(T)[1]
     for i=1:s
         setindex!(K, T[i], i)
     end
     return K
 end
-    
-function *(S::Vector{<:types}, w::Matrix{<:numberTypes})
-    K = copy(S)
-    s = size(w)[2]
+
+
+function *( S::Vector{<:types}, w::Matrix{<:numberTypes})
+    element = deepcopy(S[1])
+    type = typeof(element) 
+    K = Vector{type}([element])
+    T = toNumberMatrix(S)
+    T = T * w
+    s = size(T)[2]
     for i=1:s
-        if(i <= size(K)[1])
-            K[i] = S * w[:,i]
-        else
-            push!(K, S * w[:,i])
+        for j=1:size(T)[1]
+            K[i][j] = T[j,i]
+        end
+        if( i < s)
+            push!(K, deepcopy(element))
         end
     end
     return K
 end
+    
 
 function toNumberMatrix(S::Vector{<:types})
     K = zeros(size(S[1])[1], size(S)[1])

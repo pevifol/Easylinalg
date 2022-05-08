17a85f15827c9732686dd63588d69cda316f0a64
Bruno Hryniewicz <brunohry@dcc.ufrj.br>
2022-05-07 21:17:19 -0300

    terminaid testes

--- test/point3D/point3DTeste.jl
+++ test/point3D/point3DTeste.jl
@@ -16,15 +16,14 @@
         @test_throws ErrorException Point3D(1.0, 2.0, 3)[4]
         @test_throws ErrorException Point3D(1.0, 2.0, 3)[4] = 3.0
     end;
-    # @testset "EasyLinalg" begin
-    #     @test convex_combination( [ Point3D(1.0, 2.0), Point3D(4.0, 8.0) ], 0.5) == [ Point3D(1.0, 2.0), Point3D(2.5, 5), Point3D(4.0, 8.0) ]
-    #     @test toNumberVector(Point3D(1.0, 2.0)) == [1.0, 2.0]
-    #     @test toNumberMatrix( [ Point3D(1.0, 2.0), Point3D(4.0, 5.0) ] ) == [1.0 4.0; 2.0 5.0]
-    #     @test ([ Point3D(1.0, 2.0), Point3D(4.0, 5.0) ]) * [ 1.0 0.0; 0.0 1.0] ==  [ Point3D(1.0, 2.0), Point3D(4.0, 5.0) ]
-    #     @test ([ Point3D(1.0, 2.0), Point3D(4.0, 5.0) ]) * [ 2.0, 3.0 ] ==  Point3D(14.0, 19.0)
-    #     @test ([ Point3D(1.0, 2.0), Point3D( 4.0, 5.0) ]) * [ 2.0 3.0; 4.0 5.0 ] == [ Point3D(18.0, 24.0), Point3D( 23.0, 31.0 ) ]
-    #     @test ([ Point3D(1.0, 2.0), Point3D( 4.0, 5.0) ]) \ (([ Point3D(1.0, 2.0), Point3D( 4.0, 5.0) ]) * [ 2.0 3.0; 4.0 5.0 ]) == [ 2.0 3.0; 4.0 5.0 ]
-    #     @test ([ Point3D(1.0, 2.0), Point3D( 4.0, 5.0), Point3D(7.0, 8.0) ]) \ (([ Point3D(1.0, 2.0), Point3D( 4.0, 5.0), Point3D( 7.0, 8.0) ]) * [ 2.0 3.0; 4.0 5.0; 6.0 7.0]) ≈ [ 2.0 3.0; 4.0 5.0; 6.0 7.0]
-    # end;
+    @testset "EasyLinalg" begin
+        @test convex_combination( [ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 ) ], 0.5) == [ Point3D(1.0, 2.0, 3.0), Point3D(2.0, 4.0, 6.0 ), Point3D(3.0, 6.0, 9.0 ) ]
+        @test toNumberVector(Point3D(1.0, 2.0, 3.0)) == [1.0, 2.0, 3.0]
+        @test toNumberMatrix( [ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0) ] ) == [1.0 3.0; 2.0 6.0; 3.0 9.0]
+        @test ([ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 )  ]) * [ 1.0 0.0; 0.0 1.0] ==  [ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 )  ]
+        @test ([ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 )  ]) * [ 2.0, 3.0 ] ==  Point3D(11.0, 22.0, 33.0)
+        @test ([ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 )  ]) * [ 2.0 3.0; 4.0 5.0 ] == [ Point3D(14.0, 28.0, 42.0), Point3D(18.0, 36.0, 54.0) ]
+        @test ([ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 )  ]) \ (([ Point3D(1.0, 2.0, 3.0), Point3D(3.0, 6.0, 9.0 ) ]) * [ 2.0 3.0; 4.0 5.0 ]) ≈ [ 1.4 1.8; 4.2 5.4 ]
+    end;
 end;    
 
